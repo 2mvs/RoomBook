dd007d6fd6736e050aabed81a2db54713383afd1
2mvs <119425975+2mvs@users.noreply.github.com>
2025-06-22 12:22:23 +0100

    Updated of userUpdate function

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -83,9 +83,9 @@ class AuthController extends Controller
     public function update(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users,email,' . auth()->id(),
-            'password' => 'nullable|string|min:6|confirmed',
+            'name' => 'sometimes|string|max:255',
+            'email' => 'sometimes|string|email|max:255|unique:users,email,' . auth()->id(),
+            'password' => 'sometimes|string|min:6|confirmed',
         ]);
 
         $user = auth()->user();
